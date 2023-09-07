9cde84373756caf777f2141f20980ccc0ba48be4
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-09-07 10:04:08 +0100

    Updating initial commit

--- src/main/java/com/james/eventbooking_hub/controller/MailController.java
+++ src/main/java/com/james/eventbooking_hub/controller/MailController.java
@@ -2,28 +2,34 @@ package com.james.eventbooking_hub.controller;
 import com.james.eventbooking_hub.model.Users;
 import com.james.eventbooking_hub.service.MailService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.james.eventbooking_hub.service.MailService.TEST_SUBJECT;
 
-@AllArgsConstructor
-@Controller
+@RequiredArgsConstructor
+@RestController
+@Slf4j
 @RequestMapping("/")
 public class MailController {
     private final MailService mail;
 
-    @PostMapping("/")
-    public ResponseEntity<String> sendEmail(@RequestBody Users users) {
+    @PostMapping("send")
+    public ResponseEntity<String> sendEmail1(@RequestBody Users users) {
         try {
+            log.info("i got here " + users.getEmail());
             mail.sendEmail(users.getEmail(), MailService.TEST_SUBJECT, MailService.TEST_CONTENT);
             return ResponseEntity.ok("Email sent Successfully");
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatusCode.valueOf(500));
+            log.info("i'm doing this instead");
+            return new ResponseEntity<>(HttpStatusCode.valueOf(401));
         }
     }
 }
--- src/main/java/com/james/eventbooking_hub/service/MailService.java
+++ src/main/java/com/james/eventbooking_hub/service/MailService.java
@@ -2,23 +2,25 @@ package com.james.eventbooking_hub.service;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-@AllArgsConstructor
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class MailService {
     private final JavaMailSender javaMailSender;
-    public static final String TEST_CONTENT="Welcome to Event Booking Nig LTD.";
-    public static final String TEST_SUBJECT="Please confirm your Booking details for your Event Ticket";
+    public static final String TEST_CONTENT="Please confirm your Booking details for your Event Ticket";
+    public static final String TEST_SUBJECT="Welcome to Event Booking Nig LTD.";
     public void sendEmail(String toEmail, String subject, String content){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(toEmail);
         mailMessage.setSubject(subject);
         mailMessage.setText(content);
+        log.info("I will send email");
         javaMailSender.send(mailMessage);
-
-
     }
 }
 
