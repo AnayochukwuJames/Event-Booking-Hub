f87ace37370e828691217785cc7c360ba10f900a
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-09-12 11:58:38 +0100

    Updating initial commit

--- src/main/java/com/james/eventbooking_hub/controller/EventController.java
+++ src/main/java/com/james/eventbooking_hub/controller/EventController.java
@@ -1,31 +1,23 @@
 package com.james.eventbooking_hub.controller;
 
-import com.james.eventbooking_hub.dto.EventDTO;
+import com.james.eventbooking_hub.dto.dto.EventDTO;
 import com.james.eventbooking_hub.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
 
-    private final EventService eventService;
-
     @Autowired
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
+    private EventService eventService;
+
+    @GetMapping("/view/event/{id}")
+    public ResponseEntity<EventDTO> viewEvent(@PathVariable Long id) {
+        return new ResponseEntity<>(eventService.viewEvent(id), HttpStatus.OK);
     }
 
-    @GetMapping("/view/{eventId}")
-    public ResponseEntity<EventDTO> viewEvent(@PathVariable Long eventId) {
-        EventDTO eventDTO = eventService.getEventById(eventId);
-        if (eventDTO == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(eventDTO);
-    }
+
 }
