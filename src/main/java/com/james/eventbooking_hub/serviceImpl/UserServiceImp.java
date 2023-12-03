8afa7b2875bb6d890617e4da77d5d78d7ccae6bc
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-12-03 15:16:40 +0100

    Updating initial commit

--- src/main/java/com/james/eventbooking_hub/serviceImpl/UserServiceImp.java
+++ src/main/java/com/james/eventbooking_hub/serviceImpl/UserServiceImp.java
@@ -5,35 +5,27 @@ import com.james.eventbooking_hub.model.User;
 import com.james.eventbooking_hub.repository.UserRepository;
 import com.james.eventbooking_hub.service.UserService;
 
-import java.util.Calendar;
-
 public class UserServiceImp implements UserService {
+    private final UserRepository userRepository;
 
-    private UserRepository userRepository;
-//    @Override
-//    public UserDto createUser(UserDto userDto) {
-//        User user = new User();
-//        user.setUsername(userDto.getUsername());
-//        user.setEmail(userDto.getEmail());
-//        userRepository.save(user);
-//        UserDto createdUserDto = new UserDto();
-//        createdUserDto.setId(user.getId());
-//        createdUserDto.setUsername(user.getUsername());
-//        createdUserDto.setEmail(user.getEmail());
-
-//        return createdUserDto;
-//    }
-    public static String idGenerator(Long id){
-        String userIdentity = String.format("%04",id);
-        Calendar calendar = Calendar.getInstance();
-        return String.valueOf(calendar.get(calendar.getWeeksInWeekYear()) + "/ESUT/" + String.valueOf("%o4d"));
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
-
-
     @Override
     public UserDto createUser(UserDto userDto) {
-        return null;
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getUsername());
+        user.setDOB(userDto.getDOB());
+        user.setPassword(userDto.getPassword());
+        user.setIdentificationNumber(userDto.getIdentificationNumber());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        User savedUser = userRepository.save(user);
+        return new UserDto();
     }
+
+
 }
 
 
